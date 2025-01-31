b32195948321ee51db6ae857bcfe34ca122e71cb
Ali Samadi <alisamadi.096@gmail.com>
2025-01-31 11:03:29 -0600

    Write the code for counting ordered flavors

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const listflavors = prompt(
 //split the string into an array
 const flavors = listflavors.split(",");
 //console log the array of flavors
-
+console.log(flavors);
 // create an Object to store and keep the count of how many orders there are of each flavor
-
+const flavorCount = {};
 //use a function to assign a value to each flavor in the flavorcount Object and return the flavorCount Object
-
+function countFlavors(flavors) {
+  flavors.forEach((flavor) => {
+    if (flavorCount[flavor] === undefined) {
+      flavorCount[flavor] = 1;
+    } else {
+      flavorCount[flavor]++;
+    }
+  });
+  return flavorCount;
+}
 //call the function and pass the flavors array as an argument and console the result with the count of each flavor with the number of orders as a table
 console.table(countFlavors(flavors));
